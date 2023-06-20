925266faf5e8e1ee6004ec531909d6a076635b7a
HamdiYILMAZ <hamdiyilmaz85@gmail.com>
2023-06-21 02:22:27 +0300

    loading eklendi

---
+++ static/assets/js/loader.js
@@ -0,0 +1,8 @@
+jQuery(window).on("load", function () {
+  "use strict";
+
+  /*  ===================================
+        Loading Timeout
+     ====================================== */
+  $(".loader-area").fadeOut(1000);
+});
